c69aa6a846cc1730cca0363437a1d24651ad17b9
GLinBoy <hojjatabedie@gmail.com>
2021-02-23 22:53:15 +0330

    📦 NEW: Init page layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,47 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page class="q-pa-md">
+    <div class="row justify-center">
+      <div class="col-sm-12 col-md-6">
+        <div class="row text-right">
+          <div class="col-12">
+            LCD
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-12">
+            <div class="row">
+              <div class="col-sm-3">C</div>
+              <div class="col-sm-3">+/-</div>
+              <div class="col-sm-3">%</div>
+              <div class="col-sm-3">÷</div>
+            </div>
+            <div class="row">
+              <div class="col-sm-3">7</div>
+              <div class="col-sm-3">8</div>
+              <div class="col-sm-3">9</div>
+              <div class="col-sm-3">×</div>
+            </div>
+            <div class="row">
+              <div class="col-sm-3">4</div>
+              <div class="col-sm-3">5</div>
+              <div class="col-sm-3">6</div>
+              <div class="col-sm-3">-</div>
+            </div>
+            <div class="row">
+              <div class="col-sm-3">1</div>
+              <div class="col-sm-3">2</div>
+              <div class="col-sm-3">3</div>
+              <div class="col-sm-3">+</div>
+            </div>
+            <div class="row">
+              <div class="col-sm-6">0</div>
+              <div class="col-sm-3">.</div>
+              <div class="col-sm-3">=</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
